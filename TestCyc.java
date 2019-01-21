c0a1f4b520f4561fbc1d540aeba4e244d7c2eb28
anshu247 <46483372+anshu247@users.noreply.github.com>
2019-01-21 15:36:31 +0530

    in class program1

---
+++ TestCyc.java
@@ -0,0 +1,20 @@
+class TestCyc{
+	public static void main(String args[]){
+
+
+	Cyclinder c1=new Cyclinder();
+	System.out.println(c1.getcolor());
+	System.out.println(c1.getradii());
+	System.out.println(c1.getArea());
+	System.out.println(c1.getvolume());
+
+
+	Cyclinder c2=new Cyclinder();
+c2.setradii(33);
+ c2.setcolor("Black");
+	System.out.println(c2.getradii());
+	System.out.println(c2.getArea());
+	System.out.println(c2.getvolume());
+
+}
+ }
